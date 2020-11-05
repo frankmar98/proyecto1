34d2b05874200dd4a65429f8d0d2e3987a12f6a6
FMM <FMM@DESKTOP-0A18SOK>
2020-11-05 21:26:52 +0100

    2o commit fase 2

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola mundo java-git");
+		System.out.println("hola mundo java-git 2");
 	}
 
 }
